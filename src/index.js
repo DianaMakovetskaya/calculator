a29920eaeec5f5df0496d8f15a441ef1f38da0b5
Diana <diana.makovetskaya1812@gmail.com>
2021-01-07 17:01:51 +0200

    calculator

--- src/index.js
+++ src/index.js
@@ -3,61 +3,12 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import {Provider} from "react-redux";
-import {createStore} from "redux";
-
-const reducer=(state=initialState,action)=>{
-    switch (action.type){
-        case 'INC_COUNTER':{
-
-            return{
-                ...state,
-                counter: state.counter+1,
-            }
-        }
-        case 'DEC_COUNTER':{
-            return{
-                ...state,
-                counter: state.counter-1
-            }
-
-        }
-        case 'RESET':{
-            return{
-                ...state,
-                counter: 0
-            }
-        }
-        case 'CHANGE_USER':
-            return {
-                ...state,
-                user: action.payload
-            }
-
-        default:{
-            return state
-        }
-    }
-}
-
-const initialState={
-    counter:0,
-    user:{
-        name:'',
-        id:0
-    }
-
-}
-
-const store=createStore(reducer);
 
 ReactDOM.render(
-    <React.StrictMode>
-        <Provider store={store}>
-            <App/>
-        </Provider>
-    </React.StrictMode>,
-    document.getElementById('root')
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
