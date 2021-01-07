c17f1ce89b6e7155547964a5576034c09d13af3d
Diana <diana.makovetskaya1812@gmail.com>
2021-01-07 16:57:57 +0200

    calculator

---
+++ src/components/Calculator.js
@@ -0,0 +1,52 @@
+import React, {Component} from 'react';
+import './Calculator.css'
+import {useState} from 'react'
+export default function Calculator(){
+   const MyInput=React.createRef();
+
+    const [result,setResult]=useState(0)
+
+    const addOne=()=>{
+        setResult((prev)=>prev+1);
+    }
+    const subtractOne=()=>{
+        setResult((prev)=>prev-1);
+    }
+    const addHundred=()=>{
+        setResult((prev)=>prev+100);
+    }
+    const subtractHundred=()=>{
+        setResult((prev)=>prev-100);
+    }
+    const reset=()=>{
+        setResult(0);
+    }
+    const addNum=(e)=>{
+        e.preventDefault();
+        setResult((prev)=>prev+(+MyInput.current.value))
+    }
+        return (
+            <div className={'MainPage'}>
+                <div className="tittle">CALCULATOR</div>
+                <div id="result">
+                    {
+                        result && result
+                    }
+
+                </div>
+                <div className="operations">
+                    <button id={'one'} onClick={addOne}>1</button>
+                    <button id={'MinusOne'} onClick={subtractOne}>-1</button>
+                    <button id={'OneHundred'} onClick={addHundred}>100</button>
+                    <button id={'MinusOneHundred'} onClick={subtractHundred}>-100</button>
+                </div>
+                <button id={'reset'} onClick={reset}>Reset</button>
+                <form action="" name={'inputForm'} id={'inputForm'} onSubmit={addNum}>
+                    Число
+                    <input type="text" id={'inputValue'} ref={MyInput}/>
+                    <button id={'submit'}>Submit</button>
+                </form>
+            </div>
+        );
+}
+
